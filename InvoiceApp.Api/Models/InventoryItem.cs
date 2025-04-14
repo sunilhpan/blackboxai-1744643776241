d4dccbe49e8142bff9615afedc05381a4e40420a
sunilhpan <sunilhpan@gmail.com>
2025-04-14 20:47:58 +0530

    Add InvoiceApp.Api/Models/InventoryItem.cs 1744643877482

---
+++ InvoiceApp.Api/Models/InventoryItem.cs
@@ -0,0 +1,9 @@
+namespace InvoiceApp.Api.Models;
+
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
